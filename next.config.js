d06f20526b683145805e27ceb29e7f28a346fd1a
blake <blakegeist@gmail.com>
2022-12-20 18:55:01 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,20 @@
-{
+
+
+
+  /**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
     "rewrites": [{
         "source": "/:path*",
         "has": [
-          {
-            "type": "host",
-            "value": ":pet-tips-n-tricks.com"
-          }
+            {
+                "type": "host",
+                "value": ":pet-tips-n-tricks.com"
+            }
         ],
         "destination": "/sites/pet-tips-n-tricks.com/:path*"
-      }]
+    }]
   }
+  
+  module.exports = nextConfig
