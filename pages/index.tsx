cd6d8e21bf459acc6280d63bbd14d4463648afbf
blake <blakegeist@gmail.com>
2022-12-20 20:23:49 -0800

    updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,30 @@
 import React from 'react'
 import Head from 'next/head'
-import HomePage from '../components/pages/Home'
 import { firebaseDb } from 'utils/firebase'
-import { collection, getDocs, query, limit } from "firebase/firestore"
+import { doc, getDoc } from "firebase/firestore"
+import HomePage from 'components/pages/Home'
 
-const Home = ({ sites }) => (
-    <>
-      <Head>
-        <title>Geist App</title>
-        <meta name="description" content="Blake's create blog sites app" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+const Home = ({ site }) => (
+  <>
+    <Head>
+      <title>Geist App</title>
+      <meta name="description" content="Blake's create blog sites app" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
 
-      <HomePage sitesArray={sites} />
-    </>
-  )
+    <div>
+      <HomePage site={site} />
+    </div>
+  </>
+)
 
+export const getServerSideProps = async ({ req }) => {
+  const host = req.headers.host
+  const docRef = doc(firebaseDb, "sites", host)
+  const siteDoc = await getDoc(docRef)
+  const site = siteDoc.data()
 
-export const getServerSideProps = async () => {
-  const orderedDocs = query(collection(firebaseDb, "sites"), limit(100))
-  const querySnapshot = await getDocs(orderedDocs)
-  const sites = querySnapshot.docs.map(doc => doc.data())
-
-  return { props: { sites: sites || null  } }
+  return { props: { site: site || null  } }
 }
 
 export default Home
