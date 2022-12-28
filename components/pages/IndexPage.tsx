b1e586c35aa572d8e375627dba45b9f7ee3a2814
blake <blakegeist@gmail.com>
2022-12-28 00:26:08 -0800

    Update IndexPage.tsx

--- components/pages/IndexPage.tsx
+++ components/pages/IndexPage.tsx
@@ -4,28 +4,32 @@ import { FeaturedPost, Post } from 'components/Post'
 import { Heading, FeaturedCards } from 'components/pages/IndexPageStyles'
 
 const IndexPage = ({ posts }) => {
-
-    const daPosts = posts
-
     const featuredPosts = posts.slice(0, 4)
-
     const regularPosts = posts.slice(4, posts.length)
+
+    const FeaturedPosts = () => (
+        <FeaturedCards>
+            {featuredPosts.map((post, i) => {
+                return <FeaturedPost key={`${post.slug}-${i}-key`} post={post} />
+            })}
+        </FeaturedCards>
+    )
+
+    const Posts = () => (
+        <FeaturedCards>
+            {regularPosts?.map((post, i) => {
+                return <Post key={`${post.slug}-${i}-key`} post={post} />
+            })}
+        </FeaturedCards>        
+    )
 
     return (
         <Layout>
             <>
                 <Heading><span>Featured</span></Heading>
-                <FeaturedCards>
-                    {featuredPosts.map((post, i) => {
-                        return <FeaturedPost key={`${post.slug}-${i}-key`} post={post} />
-                    })}
-                </FeaturedCards>
+                <FeaturedPosts />
                 <Heading><span>All Stories</span></Heading>
-                <FeaturedCards>
-                    {regularPosts?.map((post, i) => {
-                        return <Post key={`${post.slug}-${i}-key`} post={post} />
-                    })}
-                </FeaturedCards>
+                <Posts />
             </>
         </Layout>
     )
