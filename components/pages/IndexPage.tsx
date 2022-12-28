11ad48b4419be761dee98f21eaf509920f606941
blake <blakegeist@gmail.com>
2022-12-28 08:17:33 -0800

    refactor featured posts

--- components/pages/IndexPage.tsx
+++ components/pages/IndexPage.tsx
@@ -1,35 +1,19 @@
 import React from 'react'
 import Layout from 'components/Layout'
-import { FeaturedPost, Post } from 'components/Post'
-import { Heading, FeaturedCards } from 'components/pages/IndexPageStyles'
+import { FeaturedPosts, Posts } from 'components/pages/Index/FeaturedPosts'
+import { Heading } from 'components/pages/IndexPageStyles'
 
 const IndexPage = ({ posts }) => {
     const featuredPosts = posts.slice(0, 4)
     const regularPosts = posts.slice(4, posts.length)
 
-    const FeaturedPosts = () => (
-        <FeaturedCards>
-            {featuredPosts.map((post, i) => {
-                return <FeaturedPost key={`${post.slug}-${i}-key`} post={post} />
-            })}
-        </FeaturedCards>
-    )
-
-    const Posts = () => (
-        <FeaturedCards>
-            {regularPosts?.map((post, i) => {
-                return <Post key={`${post.slug}-${i}-key`} post={post} />
-            })}
-        </FeaturedCards>        
-    )
-
     return (
         <Layout>
             <>
                 <Heading><span>Featured</span></Heading>
-                <FeaturedPosts />
+                <FeaturedPosts featuredPosts={featuredPosts} />
                 <Heading><span>All Stories</span></Heading>
-                <Posts />
+                <Posts regularPosts={regularPosts} />
             </>
         </Layout>
     )
