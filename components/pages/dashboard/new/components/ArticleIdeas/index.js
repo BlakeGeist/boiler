d70877813ce042dcc60a7df7e10f870163c29c92
blake <blakegeist@gmail.com>
2023-04-11 10:42:38 -0700

    refactor create modal

--- components/pages/dashboard/new/components/ArticleIdeas/index.js
+++ components/pages/dashboard/new/components/ArticleIdeas/index.js
@@ -2,10 +2,25 @@ import React from 'react'
 import ArticleIdea from './ArticleIdea'
 import { ArticleIdeasList } from './styles'
 
-const ArticleIdeas = ({ articleIdeas, setSelectedIdea, setPost, setStep }) => {
+const ArticleIdeas = ({ articleIdeas, setSelectedIdea, setPost, setStep, stepUp }) => {
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const customIdea = e.target.customIdea.value
+        setSelectedIdea(customIdea)
+        setPost({ heading: customIdea })
+        stepUp()
+    }
+
     return (
         <ArticleIdeasList>
+            
             {articleIdeas?.map((idea, index) => ( <ArticleIdea idea={idea} index={index} key={`${idea}`} setSelectedIdea={setSelectedIdea} setPost={setPost} setStep={setStep} /> ))}
+
+            <form onSubmit={handleSubmit}>
+                <input type="text" id="customIdea" placeholder="Enter Custom Heading" name="customIdea" />
+                <input type="submit" />
+            </form>
         </ArticleIdeasList>
     )
 }
--- components/pages/posts/post/Main.js
+++ components/pages/posts/post/Main.js
@@ -51,14 +51,14 @@ const Post = ({ post, recent_posts, categories, site }) => {
                 <Share />
                 <Body>
                     <Summary summaryRef={summaryRef} summary={post.summary} />
-                    {site.headerAd &&
+                    {site?.headerAd &&
                         <div dangerouslySetInnerHTML={{ __html: site.headerAd }} />
                     }
                     <Article articleRef={articleRef} html={splitArticle} />
                     <Categories categories={categories} />
                     <QuoteAndAd>
                         <Quote quoteRef={quoteRef} quote={post.quote} />
-                        {site.bodyAd &&
+                        {site?.bodyAd &&
                             <div dangerouslySetInnerHTML={{ __html: site.bodyAd }} />
                         }
                     </QuoteAndAd>
@@ -82,7 +82,7 @@ const Post = ({ post, recent_posts, categories, site }) => {
                     }
                 </Body>
                 <Aside
-                    sidebarAd={site.sidebarAd}
+                    sidebarAd={site?.sidebarAd}
                     topRef={topRef}
                     summaryRef={summaryRef}
                     articleRef={articleRef}
