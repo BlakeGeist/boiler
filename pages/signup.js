4d76cf4acd988a0117a2aa13062c957163dce75d
blake <blakegeist@gmail.com>
2023-04-12 23:22:34 -0700

    Update signup.js

--- pages/signup.js
+++ pages/signup.js
@@ -81,7 +81,7 @@ const SignupPage = () => {
               type="submit"
               className={`h-12 text-center w-2/3 bg-blue-900 border-2 rounded-md hover:shadow-lg hover:bg-blue-800 text-lg transition`}
             >
-              <p className="capitalize text-white font-normal">submit</p>
+              <p className="capitalize text-white font-normal">submit</p> 
             </button>
           </div>
         </form>
