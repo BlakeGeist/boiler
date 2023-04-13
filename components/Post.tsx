8d09aa45c22e33490bcd686bc513111bad6acbd8
blake <blakegeist@gmail.com>
2023-04-12 17:55:50 -0700

    Update Post.tsx

--- components/Post.tsx
+++ components/Post.tsx
@@ -24,22 +24,23 @@ export const Post = ({ post }) =>  {
         </Card>    
     )
 }
+
 export const FeaturedPost = ({ post }) => {
     return (
-    <FeaturedCard>
-        <Link href={`/posts/${post.slug}`} legacyBehavior>
-            <FeaturedCardImageLink src={post.mediumImageSrc} />
-        </Link>
-        <FeaturedCardContent>
-            <FeaturedCardText>
-                <h2>
-                    <Link href={`/posts/${post.slug}`} legacyBehavior>
-                        <a>{post.heading}</a>
-                    </Link>
-                </h2>
-                <p>{truncateString(post.shortDescription, 275)}</p>                                
-            </FeaturedCardText>
-        </FeaturedCardContent>
-    </FeaturedCard>    
-)
-    }
+        <FeaturedCard>
+            <Link href={`/posts/${post.slug}`} legacyBehavior>
+                <FeaturedCardImageLink src={post.mediumImageSrc} />
+            </Link>
+            <FeaturedCardContent>
+                <FeaturedCardText>
+                    <h2>
+                        <Link href={`/posts/${post.slug}`} legacyBehavior>
+                            <a>{post.heading}</a>
+                        </Link>
+                    </h2>
+                    <p>{truncateString(post.shortDescription, 275)}</p>
+                </FeaturedCardText>
+            </FeaturedCardContent>
+        </FeaturedCard>    
+    )
+}
