b0108ba67030036a2df3ae960d00289c6269e437
blake <blakegeist@gmail.com>
2023-04-15 01:12:49 -0700

    product campaigns update

--- components/pages/dashboard/product-campaigns/product/Main.js
+++ components/pages/dashboard/product-campaigns/product/Main.js
@@ -3,7 +3,7 @@ import React from 'react'
 const ProductMain = ({ site, product }) => {
     console.log(site, product)
     return (
-        <h2>Product main</h2>
+        <h2>{product.name}</h2>
     )
 }
 
--- pages/dashboard/product-campaigns/[product].js
+++ pages/dashboard/product-campaigns/[product].js
@@ -3,36 +3,34 @@ import Layout from 'components/Layout'
 import ProductMain from 'components/pages/dashboard/product-campaigns/product/Main'
 import Breadcrumbs from 'components/BreadCrumbs'
 import nookies from 'nookies'
-import { firebaseDb } from 'utils/firebase'
 import { firebaseAdmin } from 'utils/firebaseAdmin'
-import { collection, getDocs } from 'firebase/firestore'
+import { getDocFromPathAndSlug } from 'utils/firebase'
 
-const ProductCampaign = ({ user, products, site, host, lang }) => (
+const ProductCampaign = ({ user, product, site, host, lang }) => (
     <Layout site={site} user={user}>
         <Layout.Main>
             <Breadcrumbs pathArray={[{text: 'Dashboard', href: "/dashboard"}, {text: 'Product Campagins' }]} />
-            <ProductMain products={products} site={site} host={host} lang={lang} />
+            <ProductMain product={product} site={site} host={host} lang={lang} />
         </Layout.Main>
     </Layout>
 )
+
 
 export const getServerSideProps = async (ctx) => {
     try {
         const cookies = nookies.get(ctx)
         const host = ctx.req.headers.host
         const token  = await firebaseAdmin.auth().verifyIdToken(cookies.token)
+        const slug = ctx.query.product
 
-        const postsCollRef = collection(firebaseDb, `/sites/${host}/productCampaigns`)
-        const postsDocs = await getDocs(postsCollRef)
-        const products = postsDocs.docs?.map(d => ({id: d.id, ...d.data()})) || null
-    
+        const product = await getDocFromPathAndSlug(`sites/${host}/productCampaigns`, slug)
+
         return {
-            props: { products, user: token, host }
+            props: { product, user: token, host }
         }
     } catch (err) {
 
         console.log('err, ', err)
-
         ctx.res.writeHead(302, { Location: '/dashboard/login' })
         ctx.res.end()
 
