5a4ad3f2bbf7d2d3c24648c40747243a9ffb614f
blake <blakegeist@gmail.com>
2023-04-19 18:31:49 -0700

    init privacy policy and terms pages

---
+++ pages/privacy-policy/index.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import Layout from 'components/Layout'
+
+const Terms = ({ site }) => {
+    return (
+        <Layout site={site}>
+            <Layout.Main>
+
+                <h1>Terms of Service for Pet Adoption Website</h1>
+
+                <p>Welcome to our pet adoption website! These Terms of Service (the &#34;Terms&#34;) govern your use of our website located at [insert website URL] (the &#34;Website&#34;), which is owned and operated by [insert company name] (&#34;we&#34;, &#34;us&#34;, or &#34;our&#34;).</p>
+
+                <p>By accessing or using the Website, you agree to be bound by these Terms. If you do not agree to these Terms, you may not use the Website.</p>
+
+                <p>Pet Adoption: Our Website provides a platform for pet owners and potential adopters to connect. We do not represent or warrant the health or temperament of any pets listed on our Website, and we do not endorse any pet owners or adopters who use our platform.</p>
+
+                <p>User Content: We may allow users to submit content, such as pet profiles or adoption applications, to the Website (the &#34;User Content&#34;). By submitting User Content, you grant us a non-exclusive, transferable, sub-licensable, royalty-free, worldwide license to use, copy, modify, create derivative works based on, distribute, publicly display, publicly perform, and otherwise exploit in any manner such User Content in all formats and distribution channels now known or hereafter devised (including in connection with the Website and our business and on third-party sites and services), without further notice to or consent from you, and without the requirement of payment to you or any other person or entity.</p>
+
+                <p>You represent and warrant that: (i) you either are the sole and exclusive owner of all User Content or you have all rights, licenses, consents, and releases that are necessary to grant to us the rights in such User Content, as contemplated under these Terms; and (ii) neither the User Content, nor your submission, uploading, publishing or otherwise making available of such User Content, nor our use of the User Content as permitted herein will infringe, misappropriate or violate a third party&apos;s patent, copyright, trademark, trade secret, moral rights or other proprietary or intellectual property rights, or rights of publicity or privacy, or result in the violation of any applicable law or regulation.</p>
+
+                <p>Prohibited Conduct: You may not use the Website for any illegal or unauthorized purpose. You agree to comply with all laws, rules, and regulations applicable to your use of the Website. You may not post or transmit, or cause to be posted or transmitted, any communication or solicitation designed or intended to obtain password, account, or private information from any user of the Website.</p>
+
+                <p>Indemnification: You agree to indemnify, defend, and hold harmless us and our affiliates, officers, directors, employees, agents, and licensors from and against any and all claims, liabilities, expenses, damages, and costs (including reasonable attorneys&apos; fees) arising out of or in any way related to your use of the Website, your User Content, or your breach of these Terms.</p>
+
+                <p>Limitation of Liability: In no event shall we be liable for any indirect, incidental, special, consequential, or punitive damages, including without limitation, loss of profits, data, use, goodwill, or other intangible losses, resulting from (i) your access to or use of or inability to access or use the Website; (ii) any conduct or content of any third party on the Website; (iii) any content obtained from the Website; or (iv) unauthorized access, use, or alteration of your transmissions or content.</p>
+
+                <p>Governing Law: These Terms shall be governed by and construed in accordance with the laws of the jurisdiction where our company is located, without giving effect to any principles of conflicts of law.</p>
+
+                <p>Changes to the Terms: We reserve the right to modify or update these Terms from time to time, in our sole discretion. If we make any material changes to these Terms, we will notify you by posting a notice on the Website or by sending an email to the email address associated with your account. Your continued use of the Website after any such changes constitutes your acceptance of the new Terms.</p>
+
+                <p>Termination: We reserve the right, without notice and in our sole discretion, to terminate your access to the Website, for any reason or no reason, including if we believe that you have violated or acted inconsistently with these Terms.</p>
+
+                <p>Miscellaneous: These Terms constitute the entire agreement between you and us regarding the use of the Website. If any provision of these Terms is found to be invalid or unenforceable, the remaining provisions shall remain in full force and effect. Our failure to enforce any right or provision of these Terms shall not be deemed a waiver of such right or provision. You may not assign or transfer these Terms, by operation of law or otherwise, without our prior written consent. We may assign these Terms at any time without notice. These Terms do not create any agency, partnership, joint venture, or employment relationship, and you may not make any representations on our behalf.</p>
+
+            </Layout.Main>
+            <Layout.Aside />
+        </Layout>        
+    )
+}
+
+export default Terms
