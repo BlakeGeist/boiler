c5ec2b04b82d3514037816127f4f96d3c8f97d9b
blake <blakegeist@gmail.com>
2023-04-20 10:11:07 -0700

    update scheduled post for campaign posts

--- components/pages/dashboard/product-campaigns/product/components/ArticleIdeas.js
+++ components/pages/dashboard/product-campaigns/product/components/ArticleIdeas.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import ArticleIdea from './ArticleIdea'
 
-const ArticleIdeas = ({ articleIdeas }) => {
+const ArticleIdeas = ({ articleIdeas, host, lang }) => {
     if(!articleIdeas || articleIdeas.length === 0) return null
 
     return (
         <>
             <h2>Article Ideas</h2>
             {articleIdeas.map((articleIdea) => (
-                <ArticleIdea articleIdea={articleIdea} key={`${articleIdea.title}-checkbox`} />
+                <ArticleIdea articleIdea={articleIdea} key={`${articleIdea.title}-checkbox`} host={host} lang={lang} />
             ))}
         </>        
     )
