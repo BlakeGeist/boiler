deb4580bd4e239f8c821d16704730a30e35d24ff
blake <blakegeist@gmail.com>
2023-04-20 22:34:03 -0700

    Update PostTableRow.js

--- components/pages/dashboard/posts/components/PostTableRow.js
+++ components/pages/dashboard/posts/components/PostTableRow.js
@@ -4,6 +4,26 @@ import Button from '@mui/material/Button'
 import { doc, deleteDoc } from "firebase/firestore"
 import { firebaseDb } from 'utils/firebase'
 import { LoadingButton } from '@mui/lab'
+import styled from 'styled-components'
+
+const PostHas = styled.ul`
+    padding: 0;
+    font-size: 10px;
+
+    li {
+        display: inline-block;
+    }
+`
+
+const PostHasItem = styled.li`
+    color: ${props => props.hasItem ? "black" : "red"};
+    font-weight: ${props => props.hasItem ? "normal" : "bold"};
+    padding: 0 5px;
+
+    &:first-of-type {
+        padding-left: 0;
+    }
+`
 
 const PostTableRow = ({ setPosts, posts, host, post, lang }) => {
     const [isLoading, setIsLoading] = useState(false)
@@ -24,6 +44,15 @@ const PostTableRow = ({ setPosts, posts, host, post, lang }) => {
         <tr key={post.slug}>
             <td>
                 <Link href={`/posts/${post.slug}`}>{post.heading}</Link>
+            </td>
+            <td>
+                <PostHas>
+                    <PostHasItem hasItem={post.articleHtml.length > 0}>Article</PostHasItem>
+                    <PostHasItem hasItem={post.headerImageSrc.length > 0}>Header Image</PostHasItem>
+                    <PostHasItem hasItem={post.mediumImageSrc.length > 0}>Body Image</PostHasItem>
+                    <PostHasItem hasItem={post.faqs.length > 0}>Faqs</PostHasItem>
+                    <PostHasItem hasItem={post.listicleItems.length > 0}>Listicle</PostHasItem>
+                </PostHas>
             </td>
             <td>
                 <Link href={`/posts/${post.slug}`} target="_blank">
