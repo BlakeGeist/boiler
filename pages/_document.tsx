db1567288598c9b3a87d6c65fa2a2ec428181abd
blake <blakegeist@gmail.com>
2023-05-01 22:51:30 -0700

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 export default class MyDocument extends Document {
     render () {
         return (
-            <Html lang="en">
+            <Html>
                 <Head>
                     <meta charSet="utf-8" />
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
