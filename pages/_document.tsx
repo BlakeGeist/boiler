a0799aea8a18e2586e95c2355aa23ca4fa1e266b
blake <blakegeist@gmail.com>
2023-05-16 11:29:30 -0700

    update adsense call

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,31 +1,10 @@
 import React from 'react'
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Main, NextScript } from 'next/document'
 
 export default class MyDocument extends Document {
     render () {
         return (
             <Html lang="en">
-                <Head>
-                    <meta charSet="utf-8" />
-                    <link rel="preconnect" href="https://fonts.googleapis.com" />
-                    <link rel="preconnect" href="https://fonts.gstatic.com" />
-                    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet" />                    
-                    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7240508925750002" crossOrigin="anonymous"></script>
-                
-                    <script
-                        dangerouslySetInnerHTML={{
-                        __html: `
-                            window.dataLayer = window.dataLayer || [];
-                            function gtag(){dataLayer.push(arguments);} 
-                            gtag('js', new Date());
-
-                            gtag('config', 'G-HMMSNDKE0N');
-                        `
-                        }}
-                    />
-                    
-                    <script async src="https://www.googletagmanager.com/gtag/js?id=G-HMMSNDKE0N"></script>
-                </Head>
                 <body>
                     <Main />
                     <NextScript />
