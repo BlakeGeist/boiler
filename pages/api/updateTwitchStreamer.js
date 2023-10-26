2786690f625710c28faf11da8ac568c3ec69f88b
blake <blakegeist@gmail.com>
2023-10-26 06:54:13 -0700

    Update updateTwitchStreamer.js

--- pages/api/updateTwitchStreamer.js
+++ pages/api/updateTwitchStreamer.js
@@ -25,7 +25,7 @@ export default async function handler(req, res) {
             })
         }
 
-        if(currentDate < twitchHighlight[0].endFeatureAt) {
+        if(currentDate > twitchHighlight[0].endFeatureAt) {
             return res.status(200).json({
                 updateStatus: 'no update, current user is still within their uptime',
                 currentlyFeaturedStreamer
